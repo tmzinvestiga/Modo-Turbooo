5ce15179f316ef58b40ee167f970dad0d2d98584
tmzinvestiga <tmz.investiga@gmail.com>
2025-06-20 18:45:00 -0300

    teste

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDS5trGnp4xdIhJoeyCRh6FOCs79bKXvQ0",
+  authDomain: "modo-turbo-a0739.firebaseapp.com",
+  projectId: "modo-turbo-a0739",
+  storageBucket: "modo-turbo-a0739.firebasestorage.app",
+  messagingSenderId: "339613445345",
+  appId: "1:339613445345:web:96292786ed30cb8d121ba0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
